3ed1aafdf8a6feb70c61839ac785f303d302d516
koji333 <koji333@me.com>
2018-12-13 20:28:57 +0900

    課題３
    テキストがでない

--- camera/processor/motion_detector.py
+++ camera/processor/motion_detector.py
@@ -52,8 +52,8 @@ class MotionDetector(object):
 
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            
+            # cv2.putText(img, text, org, fontFace, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
             font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
-        
+            # cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+             cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255))
         return frame
