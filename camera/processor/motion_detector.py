479758793332cd01ab101df332ff322d76b353cb
koji333 <koji333@me.com>
2018-12-13 20:25:24 +0900

    Update motion_detector.py

--- camera/processor/motion_detector.py
+++ camera/processor/motion_detector.py
@@ -52,5 +52,8 @@ class MotionDetector(object):
 
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(frame,'OpenCV',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
         
         return frame
